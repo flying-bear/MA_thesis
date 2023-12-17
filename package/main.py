79ea8a2132a1fed5767e95e7b8af29874166eb6b
Galina Ryazanskaya <galka1999@gmail.com>
2023-12-17 17:08:06 +0100

    modify processing for layered pd data frame

--- package/main.py
+++ package/main.py
@@ -306,119 +306,64 @@ def pipe_texts(texts: List[str], config: Config) -> Iterable[Dict[str, Dict[str,
         yield processed.values
 
 
-def values_to_flat_dict(values: Dict[str, Dict[str, float]]) -> Dict:
+def values_to_flat_dict(values: Dict[str, Dict[str, float]], index: Optional[str] = None) -> Dict:
+    # TODO: replace with something like pd.DataFrame.from_dict(ex_1, orient='index').stack().to_frame()
     d_p = {}
     for dd in values.values():
         if type(dd) == dict:
             d_p.update(dd)
+    if index:
+        d_p['index'] = index
     return d_p
 
 
-def main():
-    # some_large_text = "Der Dackel zeichnet sich durch niedrige, kurzläufige, langgestreckte, aber kompakte Gestalt " \
-    #                   "aus. Er ist sehr muskulös, mit aufrechter Haltung des Kopfes und aufmerksamem " \
-    #                   "Gesichtsausdruck. Die verkürzten Beine der Dackel sind das Resultat einer gezielten Selektion " \
-    #                   "auf Chondrodysplasie und sind im Rassestandard verankert. Es gibt die Hunde in verschiedenen " \
-    #                   "Größen und Fellvariationen: Langhaar, Rauhaar, Kurzhaar in jeweils vielen Farbvariationen, " \
-    #                   "auch mehrfarbig, gestromt oder gefleckt. Während gefleckte Tiere als eine der 4 Färbungen " \
-    #                   "durch den FCI definiert sind, werden Schwarze Tiere ohne Brand sowie weiße mit oder ohne Brand " \
-    #                   "im Rassestandard der FCI ausdrücklich ausgeschlossen. Unter AKC-Regeln können weiß " \
-    #                   "gescheckte Dackel ausgestellt werden und sind dort als Piebalds bekannt. Die hängenden " \
-    #                   "Ohren sind nicht zu weit vorne angesetzt, ausreichend, aber nicht übertrieben lang und " \
-    #                   "abgerundet. In den drei Haararten werden die Dackel im FCI-Standard nach ihrer Größe " \
-    #                   "unterschieden in Teckel (T) (früher Normalteckel), Brustumfang (BU) über 35 cm, " \
-    #                   "Gewichtsobergrenze etwa 9 kg, Zwergteckel (Zw), BU über 30 bis 35 cm, und Kaninchenteckel (" \
-    #                   "Kt), BU bis 30 cm. "
-    # data_de = TextData(text=some_large_text, config=Config(lang='de'))
-    #
-    # some_large_text_ru = "Знаете ли вы? Будапештский университет иудаики, около 1890 года Социалистическая власть не " \
-    #                      "мешала работе раввинской семинарии (на илл.). Будапештский университет иудаики был открыт в " \
-    #                      "1877 году, через несколько десятилетий после того, как в Падуе, Меце, Париже и Бреслау были " \
-    #                      "построены первые европейские раввинские семинарии. Тем не менее, он остаётся старейшим " \
-    #                      "существующим учебным заведением в мире, где готовят раввинов. "
-    # data_ru = TextData(text=some_large_text_ru, config=Config(lang='ru'))
-    #
-    # dd = ProcessTextData(data_de)
-    # print(dd.values)
-    #
-    # dr = ProcessTextData(data_ru)
-    # print(dr.values)
+def dict_to_layered_pd(d: Dict[str, Dict[str, float]]) -> pd.DataFrame:
+    return pd.DataFrame([pd.DataFrame.from_dict(d, orient='index').stack()])
 
-    start = datetime.now()
-    config_de = Config(lang='de')
+
+def create_layered_df_for_nan() -> pd.DataFrame:
+    empty_df = pd.DataFrame([np.nan])
+    empty_df.columns = pd.MultiIndex.from_tuples([('type', 'metric')])
+    return empty_df
+
+
+def main():
     df = pd.read_csv('/Users/galina.ryazanskaya/Downloads/thesis?/code?/split_questions_cp_0.tsv', sep='\t',
                      index_col=0).dropna()
-    df.reset_index(inplace=True)
-    long = df.melt(id_vars=['index'], value_name='transcript', var_name='task').sort_values(['index', 'task'])
-    texts = long['transcript'].to_list()
-    print('time for read and melt:', datetime.now() - start)
+    config_de = Config(lang='de')
 
-    start = datetime.now()
-    vals = list(pipe_texts(texts, config_de))
-    print('time for pipe:', datetime.now() - start)
-    long['values'] = vals
 
     start = datetime.now()
     for_pd = []
-    for_entire_pd = []
-    cur_vals = ['', []]
-    for i, (ix, row) in enumerate(long.iterrows()):
-        if row['task'] == 'preprocessed_transcript':
-            dp = values_to_flat_dict(row['values'])
-            for_entire_pd.append(dp)
-        else:
-            if i > 0 and row['index'] != long.iloc[i-1]['index']:
-                vals = average_values(cur_vals[-1])
-                vals = values_to_flat_dict(vals)
-                vals['index'] = cur_vals[0]
-                for_pd.append(vals)
-                cur_vals = ['', []]
+    for index, row in df.iterrows():
+        task_values = {}
+        to_average = []
+        for k, w in row.items():
+            if pd.isnull(w):
+                v_df = create_layered_df_for_nan()
             else:
-                cur_vals[0] = row['index']
-                cur_vals[-1].append(row['values'])
-    new_df = pd.DataFrame(for_pd)
-    print('time for pd:', datetime.now() - start)
+                v = ProcessTextData(TextData(text=w, config=config_de)).values
+                if k != 'preprocessed_transcript':
+                    to_average.append(v)
+                v_df = dict_to_layered_pd(v)
+            task_values[k] = v_df
+        task_values['averaged'] = dict_to_layered_pd(average_values(to_average))
+        task_df = pd.concat(task_values.values(), keys=task_values.keys(), names=["TASK"], axis=1)
+        for_pd.append(task_df)
+    new_df = pd.concat(for_pd, axis=0)
+    new_df.index = df.index
 
-    # values_df = pd.pivot(long, columns='task', index='index')['values']
-    # print(values_df.head())
+    with open('NET_NAP_new_try.tsv', 'w') as f:
+        f.write(new_df.to_csv(sep='\t'))
+
+    # for_pd = []
+    # for index, w in df['preprocessed_transcript'].items():
+    #     v = ProcessTextData(TextData(text=w, config=config_de)).values
+    #     for_pd.append(values_to_flat_dict(v, index=index))
+    # new_df = pd.DataFrame(for_pd)
     #
-    # averages = values_df[['anger', 'sadness', 'fear', 'happiness']].apply(average_values, axis=1)
-    # print(averages)
-
-    start = datetime.now()
-    newrows = {}
-    for index, row in df[['anger', 'sadness', 'fear', 'happiness']].iterrows():
-        vals = []
-        for w in row[:-1]:
-            v = ProcessTextData(TextData(text=w, config=config_de)).values
-            vals.append(v)
-        newrows[index] = average_values(vals)
-
-    for_pd = []
-    for i, d in newrows.items():
-        d_p = {'index': i}
-        for dd in d.values():
-            d_p.update(dd)
-        for_pd.append(d_p)
-    new_df = pd.DataFrame(for_pd)
-
-    with open('NET_NAP_new_graph_limit100.tsv', 'w') as f:
-        f.write(new_df.to_csv(sep='\t'))
-
-    new_rows_entire = {}
-    for index, w in df['preprocessed_transcript'].items():
-        new_rows_entire[index] = ProcessTextData(TextData(text=w, config=config_de)).values
-
-    for_pd = []
-    for i, d in new_rows_entire.items():
-        d_p = {'index': i}
-        for dd in d.values():
-            d_p.update(dd)
-        for_pd.append(d_p)
-    new_df = pd.DataFrame(for_pd)
-
-    with open('NET_NAP_new_graph_limit100_entire.tsv', 'w') as f:
-        f.write(new_df.to_csv(sep='\t'))
+    # with open('NET_NAP_new_graph_limit100_entire.tsv', 'w') as f:
+    #     f.write(new_df.to_csv(sep='\t'))
 
     print('time for non-pipe + pd:', datetime.now() - start)
 
